3de1e71ede7c38dafb655bd9627c4af0a96b1525
Tùng <letung49222@gmail.com>
2024-04-24 20:25:52 +0700

    add store

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,21 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { useRef } from "react";
+import { AppStore, makeStore } from "@/lib/store";
+import { Provider } from "react-redux";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
   return (
-    <AntdRegistry>
-      <NextUIProvider>{children}</NextUIProvider>
-    </AntdRegistry>
+    <Provider store={storeRef.current}>
+      <AntdRegistry>
+        <NextUIProvider>{children}</NextUIProvider>
+      </AntdRegistry>
+    </Provider>
   );
 }
