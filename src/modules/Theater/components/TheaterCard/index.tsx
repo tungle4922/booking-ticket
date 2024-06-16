efd552316df5f6116333cda3a26da8126ae7693b
hiepnt284 <hiepyolo02@gmail.com>
2024-06-16 23:31:07 +0700

    update

--- src/modules/Theater/components/TheaterCard/index.tsx
+++ src/modules/Theater/components/TheaterCard/index.tsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import styles from "./theaterCard.module.css";
 import Link from "next/link";
 import unidecode from "unidecode";
+import { FacebookShareButton } from "react-share";
 
 type Props = {
   name: string;
@@ -17,6 +19,10 @@ const slugify = (text: string): string => {
 
 const TheaterCard = (props: Props) => {
   const slug = slugify(props.name);
+
+  const shareUrl =
+    "https://www.bhdstar.vn/rap-phim/"
+  const title = "BHD CINEMA";
 
   return (
     <div className={styles.container}>
@@ -42,9 +48,11 @@ const TheaterCard = (props: Props) => {
         <Link href="/theater/[name]" as={`/theater/${slug}`}>
           <span className={styles.btnDetail}>THÔNG TIN CHI TIẾT</span>
         </Link>
-        <a href="" className={styles.btnShare}>
-          <span className={styles.spanShare}>CHIA SẺ</span>
-        </a>
+        <FacebookShareButton url={shareUrl} title={title}>
+          <a href="" className={styles.btnShare}>
+            <span className={styles.spanShare}>CHIA SẺ</span>
+          </a>
+        </FacebookShareButton>
       </div>
     </div>
   );
